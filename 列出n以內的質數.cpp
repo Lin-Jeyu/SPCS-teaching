6b9f3d0f70a7bd89cb0e8411231a15c096c6d79a
Lin-Jeyu <109563963+Lin-Jeyu@users.noreply.github.com>
2023-04-09 16:23:11 +0800

    Update 列出n以內的質數.cpp

--- 列出n以內的質數.cpp
+++ 列出n以內的質數.cpp
@@ -4,16 +4,16 @@ main()
     int is_pn[101];
     int n,j=2;
     scanf("%d",&n);
-    for(int i=0;i<100;i++)
+    for(int i=0;i<=100;i++)
         is_pn[i]=1;
     while(j*j<=n){
         while(is_pn[j] == 0){
             j++;
         }
-        for(int i=j+j;i<=n;i+=i){   //本本不用 =0，所以是i=2*j
+        for(int i=j+j;i<=n;i+=j){   //本身不用 = 0，所以是i=2*j
             is_pn[i]=0;
-            j++;    //要讓它繼續動啊
         }
+        j++;//要讓它繼續動啊
     }
     for(int i=2;i<=n;i++){
         if(is_pn[i] == 1){
